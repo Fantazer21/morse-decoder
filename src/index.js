8990805b5ae8b288bf5cceb55e83e1e90ad32a2f
Ilya21 <6967221@gmail.com>
2020-10-21 19:58:01 +0200

    feat: final decision for git

--- src/index.js
+++ src/index.js
@@ -35,10 +35,18 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
+
 function decode(expr) {
-    // write your solution here
+     let res = ''
+    for (let i = 0; i < expr.length; i += 10) {
+                //from 0 to 10 cut         //replace 00 global and other letters
+      let string = expr.slice(i, i+10).replace(/00/g, '').replace(/11/g, '-').replace(/10/g, '.')
+       res += MORSE_TABLE[string]
+    }
+    return res
 }
 
 module.exports = {
